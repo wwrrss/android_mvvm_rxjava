2debbf7ee4923370184f31f573277c2fe9456688
William Rodriguez <wwrrss@gmail.com>
2019-09-03 19:11:51 -0400

    removed unit test

--- app/src/test/java/com/example/sweatworks/UserRepositoryTest.kt
+++ app/src/test/java/com/example/sweatworks/UserRepositoryTest.kt
@@ -3,40 +3,19 @@ package com.example.sweatworks
 import com.example.sweatworks.data.api.UsersApi
 import com.example.sweatworks.data.repositories.UserRepository
 import com.example.sweatworks.data.repositories.UserRepositoryImp
+import com.example.sweatworks.models.Name
 import com.example.sweatworks.models.Result
 import com.example.sweatworks.models.User
 import com.example.sweatworks.utils.Globals
 import io.reactivex.Observable
 import org.junit.Before
 import org.junit.Test
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import retrofit2.converter.gson.GsonConverterFactory
 
 class UserRepositoryTest {
 
-    lateinit var userRepository: UserRepository
-
-    @Before
-    fun setup(){
-        val mockedUserApi = MockedUserApi()
-        userRepository = UserRepositoryImp(mockedUserApi)
-    }
-
-    @Test
-    fun shouldReturnMockedResults(){
-        userRepository.fetchUsersByPage(1,1,Globals.API_SEED)
-            .test()
-            .assertNoErrors()
-            .assertComplete()
-            .assertValue {
-                it.results.isNotEmpty()
-            }
-    }
+  
 }
 
-class MockedUserApi: UsersApi {
-
-    override fun getUsersByPage(page: Int, results: Int, seed: String): Observable<Result> {
-        val user = User("test@test.com")
-        val result = Result(arrayOf(user))
-        return Observable.just(result)
-    }
-}
